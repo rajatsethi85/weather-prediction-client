149e436b32b026162aeba6c222eda0f86393d5d3
rajatsethi85 <sethirajat85@gmail.com>
2023-10-29 22:14:05 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://a593a8352e95e4961a4718af73c9bcc6-472627972.ap-south-1.elb.amazonaws.com:8081/",
+  apiBaseUrl: "http://a4331bc221a0e4ab1a50513d328cdfdc-1155597245.ap-south-1.elb.amazonaws.com:8081/",
   apiKey: "3c0pxnGmnamlsD2SDk0Psysih9yLGTU1n+QJh9AKvNMXrSFL6bYyxhYBwbla2wJT",
 };
